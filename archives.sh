94d8adfb1471f36ed48a849599786c7fcf96460a
Anders Sveen <anders@f12.no>
2010-12-07 14:39:21 +0100

    Auto deploy

--- archives.sh
+++ archives.sh
@@ -3,3 +3,4 @@ git checkout master
 zip -r solution.zip * -x "target/*" README archives.sh
 git checkout start_exercise
 zip -r assignment.zip * -x "target/*" README
+cp assignment.zip ~/Dropbox/Public/
